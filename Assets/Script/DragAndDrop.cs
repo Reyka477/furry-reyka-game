4e903fae56e477473bc379ccb7df596fa226113d
Reyka <blueorange477@gmail.com>
2025-04-01 17:25:41 +0300

    update DragAndDrop

--- Assets/Script/DragAndDrop.cs
+++ Assets/Script/DragAndDrop.cs
@@ -19,27 +19,42 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IDragHandler, IPo
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        Character character = GetComponent<Character>();
+
+        if (character != null && character.currentBattleArena != null && character.currentBattleArena.IsBattleActive)
+        {
+            Debug.Log("Нельзя перетаскивать во время боя");
+            return;
+        }
+
         startPosition = rectTransform.anchoredPosition;
         originalParent = rectTransform.parent;
 
         canvasGroup.alpha = 0.7f;
         canvasGroup.blocksRaycasts = false;
-
-        // Если начался перетаскивание героя, останавливаем текущий бой на его арене
-        Character character = GetComponent<Character>();
-        if (character.currentBattleArena != null)
-        {
-            character.currentBattleArena.StopBattle();
-        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        Character character = GetComponent<Character>();
+
+        if (character != null && character.currentBattleArena != null && character.currentBattleArena.IsBattleActive)
+        {
+            return;
+        }
+
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
+        Character character = GetComponent<Character>();
+
+        if (character != null && character.currentBattleArena != null && character.currentBattleArena.IsBattleActive)
+        {
+            return;
+        }
+
         canvasGroup.alpha = 1f;
         canvasGroup.blocksRaycasts = true;
 
@@ -49,14 +64,19 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IDragHandler, IPo
         {
             if (hoveredObject.CompareTag("Slot"))
             {
+                SlotArena slotArena = hoveredObject.GetComponent<SlotArena>();
+
+                if (slotArena != null && slotArena.battleArena != null && slotArena.battleArena.IsBattleActive)
+                {
+                    Debug.Log("Нельзя класть героя в слот на арене, где уже идет бой");
+                    break;
+                }
+
                 rectTransform.SetParent(hoveredObject.transform, false);
                 rectTransform.anchoredPosition = Vector2.zero;
 
-                SlotArena slotArena = hoveredObject.GetComponent<SlotArena>();
-                if (slotArena != null)
+                if (slotArena != null && character != null)
                 {
-                    Character character = GetComponent<Character>();
-
                     if (character.currentBattleArena != null && character.currentBattleArena != slotArena.battleArena)
                     {
                         character.currentBattleArena.heroes.Remove(character);
